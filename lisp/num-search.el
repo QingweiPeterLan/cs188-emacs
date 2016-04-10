b24b7be8ee809853b0be1f2403c56ed9eec6b5c4
qingweilan <qingweilan@gmail.com>
2016-04-10 13:42:36 -0700

    Tested out interactive programming in emacs

---
+++ lisp/num-search.el
@@ -0,0 +1,9 @@
+
+; (defun num-search-forward (NUM1 &optional NUM2 BASE)
+;   "Search forward for NUM1, or range [NUM, NUM2], in base BASE (default 10)"
+;   )
+
+(defun multiply-by-seven (number)
+	"Multiply NUMBER by seven."
+	(interactive "p")
+	(message "The result is: %d" (* 7 number)))
