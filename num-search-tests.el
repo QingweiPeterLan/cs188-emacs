45405c82ef6a7d979ad62ae30e9047cac1b7f664
qingweilan <qingweilan@gmail.com>
2016-04-10 16:33:07 -0700

    Written first ERT test for 'num-search-forward'

--- num-search-tests.el
+++ num-search-tests.el
@@ -1,1 +1,27 @@
 ;;; Tests for 'num-search-forward'
+
+; common tests
+(ert-deftest addition-test1 ()
+    (should-not (= (+ 1 2) 4)))
+(ert-deftest addition-test2 ()
+    (should (= (+ 1 2) 3)))
+(ert-deftest division-test1 ()
+    (should-error (/ 1 0)))
+
+; testing point
+(ert-deftest point-test1 ()
+    (should (= (point) 1)))
+
+; test point after insertion
+(ert-deftest buffer-test1 ()
+    (with-temp-buffer
+        (insert "Hello world!575")
+        (should (= (point) 16))))
+
+; first test for num-search-forward
+(ert-deftest buffer-test2 ()
+    (with-temp-buffer
+        (insert "Hello57 world!575")
+        (beginning-of-buffer)
+        (num-search-forward 57)
+        (should (= (point) 8))))
