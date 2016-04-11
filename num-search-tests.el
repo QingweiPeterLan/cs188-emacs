2c0e7a2668483e5774755565f89fd287ca919585
qingweilan <qingweilan@gmail.com>
2016-04-10 21:32:06 -0700

    Implemented 'num-search-forward' with only NUM1 defined

--- num-search-tests.el
+++ num-search-tests.el
@@ -1,27 +1,140 @@
 ;;; Tests for 'num-search-forward'
 
-; common tests
-(ert-deftest addition-test1 ()
-    (should-not (= (+ 1 2) 4)))
-(ert-deftest addition-test2 ()
-    (should (= (+ 1 2) 3)))
-(ert-deftest division-test1 ()
-    (should-error (/ 1 0)))
+(require 'calc-bin)
 
-; testing point
-(ert-deftest point-test1 ()
-    (should (= (point) 1)))
+; =================================================
 
-; test point after insertion
-(ert-deftest buffer-test1 ()
-    (with-temp-buffer
-        (insert "Hello world!575")
-        (should (= (point) 16))))
+; regex generation tests
+(ert-deftest regex-gen-test1 ()
+    (should (string= (generate-regex-for-num 55) "[^0-9]*+?0*55[^0-9]+")))
+(ert-deftest regex-gen-test2 ()
+    (should (string= (generate-regex-for-num -55) "[^0-9]*-0*55[^0-9]+")))
+(ert-deftest regex-gen-test3 ()
+    (should (string= (generate-regex-for-num +55) "[^0-9]*+?0*55[^0-9]+")))
+(ert-deftest regex-gen-test4 ()
+    (should (string= (generate-regex-for-num -055) "[^0-9]*-0*55[^0-9]+")))
+(ert-deftest regex-gen-test5 ()
+    (should (string= (generate-regex-for-num +055) "[^0-9]*+?0*55[^0-9]+")))
+(ert-deftest regex-gen-test6 ()
+    (should (string= (generate-regex-for-num 57) "[^0-9]*+?0*57[^0-9]+")))
 
-; first test for num-search-forward
-(ert-deftest buffer-test2 ()
+(ert-deftest regex-gen-test-base1 ()
+    (should (string= (generate-regex-for-num-with-base 255 16) "[^0-9]*+?0*FF[^0-9]+")))
+
+; =================================================
+
+; tests for num-search-forward
+(ert-deftest num-search-forward-test-1-1 ()
     (with-temp-buffer
         (insert "Hello57 world!575")
         (beginning-of-buffer)
         (num-search-forward 57)
         (should (= (point) 8))))
+
+(ert-deftest num-search-forward-test-1-2 ()
+    (with-temp-buffer
+        (insert "Hello57 world!575")
+        (beginning-of-buffer)
+        (num-search-forward +57)
+        (should (= (point) 8))))
+
+(ert-deftest num-search-forward-test-1-3 ()
+    (with-temp-buffer
+        (insert "Hello57 world!575")
+        (beginning-of-buffer)
+        (num-search-forward +000057)
+        (should (= (point) 8))))
+
+(ert-deftest num-search-forward-test-1-4 ()
+    (with-temp-buffer
+        (insert "Hello57 world!575")
+        (beginning-of-buffer)
+        (num-search-forward 000000057)
+        (should (= (point) 8))))
+
+(ert-deftest num-search-forward-test-1-5 ()
+    (with-temp-buffer
+        (insert "Hello57 world!575")
+        (beginning-of-buffer)
+        (num-search-forward -57)
+        (should (= (point) 1))))
+
+; =================================================
+
+(ert-deftest num-search-forward-test-2-1 ()
+    (with-temp-buffer
+        (insert "Hello+00057 world!575")
+        (beginning-of-buffer)
+        (num-search-forward -57)
+        (should (= (point) 1))))
+
+(ert-deftest num-search-forward-test-2-2 ()
+    (with-temp-buffer
+        (insert "Hello+00057 world!575")
+        (beginning-of-buffer)
+        (num-search-forward 57)
+        (should (= (point) 12))))
+
+(ert-deftest num-search-forward-test-2-3 ()
+    (with-temp-buffer
+        (insert "Hello-000000057 world!575")
+        (beginning-of-buffer)
+        (num-search-forward -57)
+        (should (= (point) 16))))
+
+(ert-deftest num-search-forward-test-2-4 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575")
+        (beginning-of-buffer)
+        (num-search-forward 57)
+        (should (= (point) 12))))
+
+(ert-deftest num-search-forward-test-2-5 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575")
+        (beginning-of-buffer)
+        (goto-char 7)
+        (num-search-forward 57)
+        (should (= (point) 12))))
+
+(ert-deftest num-search-forward-test-2-6 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575")
+        (beginning-of-buffer)
+        (goto-char 15)
+        (num-search-forward 57)
+        (should (= (point) 15))))
+
+(ert-deftest num-search-forward-test-2-7 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575")
+        (beginning-of-buffer)
+        (goto-char 11)
+        (num-search-forward 57)
+        (should (= (point) 11))))
+
+(ert-deftest num-search-forward-test-2-8 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575")
+        (beginning-of-buffer)
+        (goto-char 10)
+        (num-search-forward 57)
+        (should (= (point) 12))))
+
+; =================================================
+
+(ert-deftest num-search-forward-test-3-1 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575")
+        (beginning-of-buffer)
+        (goto-char 10)
+        (num-search-forward 575)
+        (should (= (point) 10))))
+
+(ert-deftest num-search-forward-test-3-2 ()
+    (with-temp-buffer
+        (insert "Hello000057 world!575\n")
+        (beginning-of-buffer)
+        (goto-char 10)
+        (num-search-forward 575)
+        (should (= (point) 22))))
--- num-search.el
+++ num-search.el
@@ -2,10 +2,57 @@
 
 (defun num-search-forward (NUM1 &optional NUM2 BASE)
   "Search for NUM1, or range [NUM1, NUM2], in base BASE (default 10)"
-  (search-forward (number-to-string NUM1))
-  )
+  (if BASE
+    ; if BASE is defined
+    (if NUM2
+      ; if NUM2 is defined
+      nil
+      ; if NUM2 is not defined
+      nil
+      )
+    ; if BASE is not defined
+    (if NUM2
+      ; if NUM2 is defined
+      nil
+      ; if NUM2 is not defined
+      (if (re-search-forward (generate-regex-for-num NUM1) nil t)
+        (if (re-search-backward (last-digit-for-num NUM1) nil t)
+          (forward-char))))))
 
-(defun multiply-by-seven (number)
-	"Multiply NUMBER by seven."
-	(interactive "nEnter the number to multiply by seven: ")
-	(message "The result is: %d" (* 7 number)))
+; used for generating regex for number
+; ex.  567
+;     +567  ==>  [^0-9]*+?0*567[^0-9]*
+;
+; ex. -567  ==>  [^0-9]*-0*567[^0-9]*
+(defun generate-regex-for-num (NUMBER)
+  "Generates regex for specified NUMBER"
+  (let ((calc-number-radix 10))
+    (if (string= (substring (number-to-string NUMBER) 0 1) "+")
+    ; number starts with "+"
+    (concat "[^0-9]*+?0*" (substring (number-to-string NUMBER) 1) "[^0-9]+")
+    ; number doesn't start with "+"
+    (if (string= (substring (number-to-string NUMBER) 0 1) "-")
+      ; number starts with "-"
+      (concat "[^0-9]*-0*" (substring (number-to-string NUMBER) 1) "[^0-9]+")
+      ; number doesn't start with "-"
+      (concat "[^0-9]*+?0*" (number-to-string NUMBER) "[^0-9]+")))))
+
+; simple helper function that extracts last digit of number
+(defun last-digit-for-num (NUMBER)
+  "Find last digit of NUMBER"
+  (substring (number-to-string NUMBER) -1))
+
+
+; used for generating regex for number in number base BASE
+(defun generate-regex-for-num-with-base (NUMBER BASE)
+  "Generates regex for specified NUMBER in number base BASE"
+  (let ((calc-number-radix BASE))
+    (if (string= (substring (number-to-string NUMBER) 0 1) "+")
+    ; number starts with "+"
+    (concat "[^0-9]*+?0*" (math-format-radix (string-to-number (substring (number-to-string NUMBER) 1))) "[^0-9]+")
+    ; number doesn't start with "+"
+    (if (string= (substring (number-to-string NUMBER) 0 1) "-")
+      ; number starts with "-"
+      (concat "[^0-9]*-0*" (math-format-radix (string-to-number (substring (number-to-string NUMBER) 1))) "[^0-9]+")
+      ; number doesn't start with "-"
+      (concat "[^0-9]*+?0*" (math-format-radix (string-to-number (number-to-string NUMBER))) "[^0-9]+")))))
